0e4ce89c8f7a099bc03c92acc07a5b7a51abbaea
Akvaro03 <alvaroballarini2010@gmail.com>
2025-02-13 15:04:18 -0300

    feature: start prototype user form

--- app/LoginPage.tsx
+++ app/LoginPage.tsx
@@ -1,9 +1,47 @@
-import { View } from "react-native";
+import AuthForm from "@/components/AuthForm";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 function LoginPage() {
-    return (<View>
-        
-    </View>);
+  return (
+    <SafeAreaView style={styles.container}>
+      <AuthForm mode="login"/>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  containerForm: {
+    display: "flex",
+    backgroundColor: "#1F2937",
+    width: "80%",
+    height: "40%",
+    borderRadius: 10,
+    borderColor: "#00ff99",
+    borderWidth: 4,
+    borderStyle: "solid",
+    justifyContent: "center",
+  },
+  containerTittle: {
+    display: "flex",
+    height: "20%",
+    alignItems: "center",
+  },
+  TittleText: {
+    color: "#00ff99",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  FormUser:{
+    height:"50%"
+  }
+});
 
 export default LoginPage;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+interface AuthFormInterface {
+  mode: "login" | "register";
+  onSubmit: (email: string, password: string, confirmPassword: string) => void;
+  toggleMode: () => void;
+}
+
+function AuthForm({ mode, onSubmit, toggleMode }: AuthFormInterface) {
+  const isLogin = mode === "login";
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        placeholderTextColor={"#bbb"}
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        placeholderTextColor={"#bbb"}
+        value={password}
+        onChangeText={setpassword}
+        secureTextEntry
+      />
+      {!isLogin && (
+        <TextInput
+          placeholder="Confirmar Contraseña"
+          placeholderTextColor={"#bbb"}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+      )}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => onSubmit(email, password, confirmPassword)}
+      >
+        <Text style={styles.buttonText}>
+          {isLogin ? "Ingresar" : "Registrarse"}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={toggleMode}>
+        <Text style={styles.switchText}>
+          {isLogin
+            ? "¿No tienes cuenta? Regístrate"
+            : "¿Ya tienes cuenta? Inicia sesión"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
+    padding: 20,
+  },
+  containerForm: {
+    width: "85%",
+    backgroundColor: "#1F2937",
+    padding: 20,
+    borderRadius: 10,
+    borderColor: "#00ff99",
+    borderWidth: 2,
+    alignItems: "center",
+  },
+  titleText: {
+    color: "#00ff99",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "#333",
+    color: "#fff",
+    padding: 10,
+    marginVertical: 8,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#00ff99",
+    paddingVertical: 12,
+    width: "100%",
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#121212",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  switchText: {
+    color: "#bbb",
+    marginTop: 15,
+    textDecorationLine: "underline",
+  },
+});
+
+export default AuthForm;
